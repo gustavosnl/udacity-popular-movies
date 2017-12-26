093cd9fbd5d9d5c3d32ef8aa2fdfea1f4911cacb
Gustavo Lima <gustavosnl@gmail.com>
2017-12-26 19:56:04 -0200

    refactoring Movie.class to implement Parcelable

--- app/src/main/java/br/com/glima/popularmovies/business/Movie.java
+++ app/src/main/java/br/com/glima/popularmovies/business/Movie.java
@@ -1,12 +1,13 @@
 package br.com.glima.popularmovies.business;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by gustavo.lima on 18/12/17.
  */
 
-public class Movie implements Serializable {
+public class Movie implements Parcelable {
 
 	private String title;
 	private String poster;
@@ -22,6 +23,24 @@ public class Movie implements Serializable {
 		this.releaseDate = releaseDate;
 	}
 
+	protected Movie(Parcel in) {
+		title = in.readString();
+		poster = in.readString();
+		synopsis = in.readString();
+		rating = in.readString();
+		releaseDate = in.readString();
+	}
+	public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+		@Override
+		public Movie createFromParcel(Parcel in) {
+			return new Movie(in);
+		}
+
+		@Override
+		public Movie[] newArray(int size) {
+			return new Movie[size];
+		}
+	};
 	public String getTitle() {
 		return title;
 	}
@@ -40,5 +59,18 @@ public class Movie implements Serializable {
 
 	public String getReleaseDate() {
 		return releaseDate;
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel parcel, int i) {
+		parcel.writeString(title);
+		parcel.writeString(poster);
+		parcel.writeString(synopsis);
+		parcel.writeString(rating);
+		parcel.writeString(releaseDate);
 	}
 }
